6dfea8551f1ca534af7a774099a75def2d189fba
zachjhavers <zachjhavers@gmail.com>
2022-12-10 15:02:01 -0500

    Third Commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -28,8 +29,19 @@ public class Main extends Application {
 	static final String USER_NAME = "COMP214_F22_er_70";
 	static final String PASS = "password";
 	ListView<String> basketItemData = new ListView<String>();
+	ListView<String> prodDescList = new ListView<String>();
+	ListView<String> prodList = new ListView<String>();
 	TextField searchBasketItemId = new TextField();
-	Button searchBaskItemId = new Button("Search"); 
+	TextField prodId = new TextField();
+	TextField prodDesc = new TextField();
+	TextField addProdDesc = new TextField();
+	TextField prodName = new TextField();
+	TextField prodImg = new TextField();
+	TextField prodPrice = new TextField();
+	TextField prodAct = new TextField();
+	Button searchBaskItemId = new Button("Search");
+	Button changeProdDescBut = new Button("Edit");
+	Button addProduct = new Button("Add");
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -40,16 +52,60 @@ public class Main extends Application {
 			root.setPadding(new Insets(25,25,25,25));
 			root.setHgap(20);
 			root.setVgap(20);
-			Label basketIdBox = new Label("Basket ID:");
-			root.add(basketIdBox, 0, 1);
-			root.add(searchBasketItemId, 1, 1);
-			root.add(searchBasketItemId, 2, 1);
-			searchBasketItemId.setOnAction(e -> {
-				getBasketData();
-			});
-			root.add(basketItemData, 0, 2);
-			GridPane.setColumnSpan(basketItemData, GridPane.REMAINING);
-			basketItemData.setPrefHeight(100);
+			//Basket Item Search
+				Label basketItemSearchLabel = new Label("Basket Item Search");
+				root.add(basketItemSearchLabel, 0, 0);
+				Label basketIdBox = new Label("Basket Item ID:");
+				root.add(basketIdBox, 0, 1);
+				root.add(searchBasketItemId, 1, 1);
+				root.add(searchBaskItemId, 2, 1);
+				searchBaskItemId.setOnAction(e -> {
+					getBasketItemData();
+				});
+				root.add(basketItemData, 0, 2);
+				GridPane.setColumnSpan(basketItemData, GridPane.REMAINING);
+				basketItemData.setPrefHeight(100);
+			//Change Product Description
+				Label labelChangeProd = new Label("Change Prodcut Description");
+				root.add(labelChangeProd, 0, 3);
+				Label labelProdId = new Label("Product ID: ");
+				root.add(labelProdId, 0, 4);
+				root.add(prodId, 1, 4);
+				Label labelProdDesc = new Label("Product Description: ");
+				root.add(labelProdDesc, 0, 5);
+				root.add(prodDesc, 1, 5);
+				root.add(changeProdDescBut, 2, 5);
+				changeProdDescBut.setOnAction(e -> {
+					changeProdDescData();
+				});
+				root.add(prodDescList, 0, 6);
+				GridPane.setColumnSpan(prodDescList, GridPane.REMAINING);
+				prodDescList.setPrefHeight(100);
+			//Add New Product
+				Label labelAddProd = new Label("Add Product");
+				root.add(labelAddProd, 0, 7);
+				Label labelProdName = new Label("Product Name: ");
+				root.add(labelProdName, 0, 8);
+				root.add(prodName, 1, 8);
+				Label labelAddProdDesc = new Label("Product Description: ");
+				root.add(labelAddProdDesc, 0, 9);
+				root.add(addProdDesc, 1, 9);
+				Label labelProdImg = new Label("Product Image: ");
+				root.add(labelProdImg, 0, 10);
+				root.add(prodImg, 1, 10);
+				Label labelProdPric = new Label("Product Price: ");
+				root.add(labelProdPric, 0, 11);
+				root.add(prodPrice, 1, 11);
+				Label labelProdAct = new Label("Product Active: ");
+				root.add(labelProdAct, 0, 12);
+				root.add(prodAct, 1, 12);
+				root.add(addProduct, 2, 12);
+				addProduct.setOnAction(e -> {
+					addNewProduct();
+				});
+				root.add(prodList, 0, 13);
+				GridPane.setColumnSpan(prodList, GridPane.REMAINING);
+				prodList.setPrefHeight(100);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -58,7 +114,7 @@ public class Main extends Application {
 	}
 	
 	//Method To Search Basket 
-	private void getBasketData() {
+	private void getBasketItemData() {
 		
 		try 
 		
@@ -67,8 +123,6 @@ public class Main extends Application {
 			Class.forName("oracle.jdbc.OracleDriver");
 
 			Connection con = DriverManager.getConnection(DATABASE_URL, USER_NAME, PASS);
-
-			Statement st = con.createStatement();
 			
 			int userBasketNum = Integer.parseInt(searchBasketItemId.getText());
 			
@@ -104,6 +158,163 @@ public class Main extends Application {
 		}
 		
 	}
+	
+	private void changeProdDescData() {
+		
+		CallableStatement cstmt = null;
+		
+		try 
+		
+		{
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			Connection con = DriverManager.getConnection(DATABASE_URL, USER_NAME, PASS);
+
+			int userProdId = Integer.parseInt(prodId.getText());
+			String userProdDesc = prodDesc.getText();
+			
+			String q = "{EXECUTE prod_desc_change(?, ?)}";
+			
+			cstmt = con.prepareCall(q);
+			
+			cstmt.setInt(1, userProdId);
+			cstmt.setString(2, userProdDesc);
+			
+			cstmt.close();
+			
+			getProductData();
+			
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void getProductData() {
+		
+		try 
+		
+		{
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			Connection con = DriverManager.getConnection(DATABASE_URL, USER_NAME, PASS);
+
+
+			String q = "SELECT IDPRODUCT, DESCRIPTION FROM BB_PRODUCT";
+			
+
+			Statement st = con.createStatement();
+
+			
+			ResultSet rs = st.executeQuery(q);
+			
+			
+	           ObservableList<String> productList = FXCollections.observableList(new ArrayList<String>());
+			   
+	           while(rs.next()) {
+	        	   
+	        	   int idProduct = rs.getInt(1);
+	        	   String prodDesc = rs.getString(2);
+	        	   
+	        	   productList.add("Product ID: "+idProduct+"\t"+"Prodcut Description: "+prodDesc);
+	           }
+	           
+	           prodDescList.setItems(productList);
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void addNewProduct() {
+		
+		CallableStatement cstmt = null;
+		
+		try 
+		
+		{
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			Connection con = DriverManager.getConnection(DATABASE_URL, USER_NAME, PASS);
+			
+			String userProdName = prodName.getText();
+			String userProdDesc = prodDesc.getText();
+			String userProdImg = prodImg.getText();
+			int userProdPric = Integer.parseInt(prodPrice.getText());
+			int userProdAct = Integer.parseInt(prodAct.getText());
+			
+			
+			String q = "{EXECUTE PROD_ADD_SP (?, ?, ?, ?, ?)}";
+			
+			cstmt = con.prepareCall(q);
+			
+			cstmt.setString(1, userProdName);
+			cstmt.setString(2, userProdDesc);
+			cstmt.setString(3, userProdImg);
+			cstmt.setInt(4, userProdPric);
+			cstmt.setInt(5, userProdAct);
+			
+			getNewProductData();
+						
+		
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void getNewProductData() {
+		
+		try 
+		
+		{
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			Connection con = DriverManager.getConnection(DATABASE_URL, USER_NAME, PASS);
+
+
+			String q = "SELECT PRODUCTNAME, DESCRIPTION, PRODUCTIMAGE, PRICE, ACTIVE FROM BB_PRODUCT";
+			
+
+			Statement st = con.createStatement();
+
+			
+			ResultSet rs = st.executeQuery(q);
+			
+			
+	           ObservableList<String> newProductList = FXCollections.observableList(new ArrayList<String>());
+			   
+	           while(rs.next()) {
+	        	   
+	        	   String prodName = rs.getString(1);
+	        	   String prodDesc = rs.getString(2);
+	        	   String prodImg = rs.getString(3);
+	        	   int prodPrice = rs.getInt(4);
+	        	   int prodAct = rs.getInt(5);
+	        	   
+	        	   newProductList.add("Product Name: "+prodName+"\t"+"Prodcut Description: "+prodDesc+"\t"+"Product Image: "+prodImg+"\t"+"Product Price: "+prodPrice+"\t"+"Product Active: "+prodAct);
+	           }
+	           
+	           prodList.setItems(newProductList);
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+	
 	
 	public static void main(String[] args) {
 		launch(args);
